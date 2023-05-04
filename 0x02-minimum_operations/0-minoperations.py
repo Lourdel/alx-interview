4010cc910ac9fc16106e90ad1a685918d53e9867
lourdel <klourdel@gmail.com>
2023-05-04 22:53:30 +0300

    Fixing min operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,44 +1,25 @@
 #!/usr/bin/python3
 """Module defines function to determine the minimum number of operations"""
 
-import math
+
+def operations_nu(number):
+    """calculates the minimum number of operations"""
+    cnt = 1
+    op_list = []
+    val = number
+    while val != 1:
+        cnt += 1
+        if val % cnt == 0:
+            while (val % cnt == 0 and val != 1):
+                val /= cnt
+                op_list.append(cnt)
+
+    return op_list
 
 
 def minOperations(n):
-    """
-    Method determines the minimum number of operations needed to result in
-    exactly nH characters in the file.
-    it returns an integer and 0 if n is impossible
-    """
-
-    if n == 1:
+    """Method Returns sum of operations until n H """
+    if n < 2 or type(n) is not int:
         return 0
-
-    primes = get_primes(int(math.sqrt(n)))
-
-    op_list = [float('inf')] * (n+1)
-    op_list[1] = 0
-
-    for i in range(2, n+1):
-        for j in primes:
-            if i % j == 0:
-                op_list[i] = min(op_list[i], op_list[j] + i//j)
-
-        if op_list[i] == float('inf'):
-            op_list[i] = i
-
-    return op_list[n] if op_list[n] != float('inf') else 0
-
-
-def get_primes(n):
-    """Method to get prime factors of n"""
-    primes = []
-    selection = [True] * (n+1)
-    for i in range(2, int(math.sqrt(n))+1):
-        if selection[i]:
-            for j in range(i*i, n+1, i):
-                selection[j] = False
-    for i in range(2, n+1):
-        if selection[i]:
-            primes.append(i)
-    return primes
+    values = operations_nu(n)
+    return sum(values)
