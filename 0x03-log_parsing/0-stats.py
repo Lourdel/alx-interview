a15dcd2bd2a4f29cfeb3b869f3709a54cb3f7dfe
lourdel <klourdel@gmail.com>
2023-05-12 05:40:08 +0300

    modified code to match the expected output

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,57 +1,42 @@
 #!/usr/bin/python3
+"""Script that reads stdin line by line and
+computes metrics"""
 
-"""script reads stdin line by line and computes metrics"""
-
-import sys
 import re
+import sys
+
+counter = 0
+file_size = 0
+
+status_codes = {200: 0, 301: 0, 400: 0,
+                401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
 
-def main():
-    """reads from stdin and computes metrics based on status codes"""
-    status_dict = {"200": 0, "301": 0, "400": 0, "401": 0,
-                   "403": 0, "404": 0, "405": 0, "500": 0}
-
-    file_size = 0
-    count = 0
-    try:
-        while True:
-            line = sys.stdin.readline()
-            if not line:
-                break
-
-            f_st = r'\[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
-            pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - ' + f_st
-
-            match = re.match(pattern, line)
-            if not match:
-                continue
-
-            status_code = match.group(3)
-
-            if not int(status_code):
-                continue
-
-            if status_code in status_dict:
-                count += 1
-                status_dict[status_code] += 1
-                file_size += int(match.group(4))
-
-            if count == 10:
-                print("File size: {}".format(file_size))
-                sorted_dict = sorted(status_dict.items(),
-                                     key=lambda x: x)
-                for key, value in sorted_dict:
-                    if value != 0:
-                        print("{}: {}".format(key, value))
-                count = 0
-    except KeyboardInterrupt as e:
-        sorted_dict = sorted(status_dict.items(),
-                             key=lambda x: x)
-        for key, value in sorted_dict:
-            if value != 0:
-                print("{}: {}".format(key, value))
-        print(e)
+def printOut(c_dict, f_size):
+    """Prints out the status codes and their number"""
+    print("File size: {}".format(f_size))
+    for key in sorted(c_dict.keys()):
+        if status_codes[key] != 0:
+            print("{}: {}".format(key, c_dict[key]))
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        for line in sys.stdin:
+            split_string = re.split('- |"|"| " " ', str(line))
+            stat_code_n_file_s = split_string[-1]
+            if counter != 0 and counter % 10 == 0:
+                printOut(status_codes, file_size)
+            counter = counter + 1
+            try:
+                status_code = int(stat_code_n_file_s.split()[0])
+                f_size = int(stat_code_n_file_s.split()[1])
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+                file_size = file_size + f_size
+            except ValueError:
+                pass
+        printOut(status_codes, file_size)
+    except KeyboardInterrupt:
+        printOut(status_codes, file_size)
+        raise
