2211c110ac90f0eeb80cfe0e06402aa37e2b41d8
lourdel <klourdel@gmail.com>
2023-05-18 19:04:29 +0300

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,7 +6,15 @@ def validUTF8(data):
     """Method checks if data is a valid UTF-8 encoding"""
     binary_data = [format(item, f"0{8}b") for item in data]
     for i in binary_data:
-        if i[0] == "0" and len(i) == 8:
-            return True
+        if i.startswith("0"):
+            continue
+        elif i.startswith("110"):
+            num_bytes = 2
+        elif i.startswith("1110"):
+            num_bytes = 3
+        elif i.startswith("11110"):
+            num_bytes = 4
         else:
             return False
+
+    return True
