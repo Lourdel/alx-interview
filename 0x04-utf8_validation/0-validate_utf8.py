ecf88a377d02bf18e9182e3d6359ccc6d6ce4b1b
lourdel <klourdel@gmail.com>
2023-05-19 05:54:06 +0300

    implementation using bit manipulation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
+
 """validate_utf8"""
 
 
 def validUTF8(data):
     """Method checks if data is a valid UTF-8 encoding"""
-    for byte in data:
-        if byte & 0x80 == 0:
-            continue
-        elif byte & 0xC0 != 0x80:
-            return False
-    return True
+    bit_state = 0
+    for num in data:
+        bit = 0b10000000
+        if not bit_state:
+            while (bit & num):
+                bit_state += 1
+                bit >>= 1
+            if bit_state > 4:
+                return False
+            if bit_state:
+                bit_state -= 1
+                if bit_state == 0:
+                    return False
+    return not bit_state
