8900bc94463532994197a89a9a4bf78c552ee3b1
lourdel <klourdel@gmail.com>
2023-06-07 22:21:33 +0300

    fixed pep8 requirements

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,13 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """nxn 2D matrix Rotation"""
+
 
 def rotate_2d_matrix(matrix):
     """Method rotates a matrix by 90 degress"""
     n = len(matrix)
-    
+
     for i in range(n):
         for j in range(i + 1, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
+
     for i in range(n):
         matrix[i] = matrix[i][::-1]
