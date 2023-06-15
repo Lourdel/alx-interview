dd68405a10e641e7795d19d9ef597899ceedd3a8
lourdel <klourdel@gmail.com>
2023-06-15 18:57:00 +0300

    Added source code files

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+    """
+    Returns the fewest number of coins needed to meet a given amount total.
+    Args:
+    coins: A list of the values of the coins in your possession.
+    total: The amount of change you need to make.
+
+    Returns:
+    The fewest number of coins needed to meet total.
+    If total is 0 or less, returns 0.
+    If total cannot be met by any number of coins you have, returns -1.
+    """
+    if total <= 0:
+        return 0
+
+    coins.sort(reverse=True)
+
+    coin_count = 0
+    for coin in coins:
+        coin_count += total // coin
+        total %= coin
+
+    if total != 0:
+        return -1
+    else:
+        return coin_count
